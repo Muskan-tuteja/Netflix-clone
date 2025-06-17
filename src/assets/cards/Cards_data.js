98192862e4f025c71ad30b9e33940bbcaa574d1a
muskan <163419476+MUSKANMAHI@users.noreply.github.com>
2025-06-17 21:39:35 +0530

    Updates

---
+++ src/assets/cards/Cards_data.js
@@ -0,0 +1,91 @@
+import card_img1 from './card1.jpg';
+import card_img2 from './card2.jpg';
+import card_img3 from './card3.jpg';
+import card_img4 from './card4.jpg';
+import card_img5 from './card5.jpg';
+import card_img6 from './card6.jpg';
+import card_img7 from './card7.jpg';
+import card_img8 from './card8.jpg';
+import card_img9 from './card9.jpg';
+import card_img10 from './card10.jpg';
+import card_img11 from './card11.jpg';
+import card_img12 from './card12.jpg';
+import card_img13 from './card13.jpg';
+import card_img14 from './card14.jpg';
+import card_img15 from './card15.jpg';
+
+const cards_data = [
+  {
+    image: card_img1,
+    name: "Fury (2014) Hindi"
+  },
+  {
+    image: card_img2,
+    name: "Wrath of the Titans (2012) Hindi"
+  },
+  {
+    image: card_img3,
+    name: "I Saw the TV Glow (2024)"
+  },
+
+  {
+    image: card_img4,
+    name: "Jewel Thief The Heist Begins 2025 "
+  },
+
+  {
+    image: card_img5,
+    name: "You"
+  },
+
+  {
+    image: card_img6,
+    name: " Gangland "
+  },
+
+  {
+    image: card_img7,
+    name: "Ayyana Mane 2025"
+  },
+
+  {
+    image: card_img8,
+    name: "MAJHAIL"
+  },
+
+  {
+    image: card_img9,
+    name: "PASIA VASOOL"
+  },
+
+  {
+    image: card_img10,
+    name: "OFFICER"
+  },
+
+  {
+    image: card_img11,
+    name: "PANGILN"
+  },
+
+  {
+    image: card_img12,
+    name: "NOTHING"
+  },
+
+  {
+    image: card_img13,
+    name: "PANGILN"
+  },
+
+  {
+    image: card_img14,
+    name: "Odela2 (2025)"
+  },
+
+  {
+    image: card_img15,
+    name: "Kesari Chapter2 "
+  },
+]
+export default cards_data;
