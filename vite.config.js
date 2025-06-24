801d3fd3de5131ce3c946c97effbf07f78505773
muskan <muskantuteja714@gmail.com>
2025-06-24 15:48:37 +0530

    24/6/25

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Netflix-clone/',
+ base: process.env.NODE_ENV === 'production' ? '/netflix-clone/' : '/',
+   plugins: [react()],
 })
 
 
